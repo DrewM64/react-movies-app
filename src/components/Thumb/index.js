6b97e761ae15efd7dda33d46f4962609bbf142a4
DrewM64 <74701517+DrewM64@users.noreply.github.com>
2022-08-10 15:28:36 -0400

    Add Routing functionality
    Added routing for Home, Movie, NotFound pages

--- src/components/Thumb/index.js
+++ src/components/Thumb/index.js
@@ -1,11 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 //Styles 
 import { Image } from "./Thumb.styles";
 
 const Thumb = ({ image, movieId, clickable }) => (
     <div>
-        <Image src={image} alt='movie-thumb'/>
+        {/* If image is clickable, present image wrapped in link component; otherwise, just show the unclickable thumbnail */}
+        {clickable ? (
+            <Link to={`/${movieId}`}>
+                <Image src={image} alt='movie-thumb'/>
+            </Link>
+        ) : (
+            <Image src={image} alt='movie-thumb'/>
+        )}
+        
     </div>
 )
 
