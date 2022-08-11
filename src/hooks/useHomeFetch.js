d4f30d2d855565125b7e053e7f5fe834b7ccc66c
DrewM64 <74701517+DrewM64@users.noreply.github.com>
2022-08-11 15:37:00 -0400

    Add sessionStorage functionality to persist application

--- src/hooks/useHomeFetch.js
+++ src/hooks/useHomeFetch.js
@@ -1,6 +1,8 @@
 import { useState, useEffect, useRef } from "react";
 //API
 import API from '../API';
+//Helpers
+import { isPersistedState } from "../helpers";
 
 const initialState = {
     page: 0, 
@@ -38,6 +40,18 @@ export const useHomeFetch = () => {
 
     // Initial and search
     useEffect(() => {
+        if (!searchTerm) {
+            const sessionState = isPersistedState('homeState');
+
+            if (sessionState) {
+                console.log("Grabbing from sessionStorage...")
+                setState(sessionState);
+                return;
+            }
+        }
+
+        console.log("Grabbing from API...")
+
         setState(initialState);
         fetchMovies(1, searchTerm)
     }, [searchTerm])
@@ -50,6 +64,13 @@ export const useHomeFetch = () => {
         setIsLoadingMore(false);
 
     }, [isLoadingMore, searchTerm, state.page])
+
+    //Write to sessionStorage
+    useEffect(() => {
+        if(!searchTerm) {
+            sessionStorage.setItem('homeState', JSON.stringify(state))
+        }
+    }, [searchTerm, state])
 
     return {state, loading, error, setSearchTerm, searchTerm, setIsLoadingMore};
 };
